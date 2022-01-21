d24a3af331964f41fed9fdb7646977f165dabb05
rabih harb <rabiharb.ss@outlook.com>
2022-01-21 17:44:10 +0200

    final fixes

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-let questionBtns = document.querySelectorAll(".question");
-let arrows = document.querySelectorAll(".arrow");
+const questionBtns = document.querySelectorAll(".question");
+const arrows = document.querySelectorAll(".arrow");
+const asnwers = document.querySelectorAll(".answer");
 
 questionBtns.forEach(function (btn, i) {
-  btn.addEventListener("click", function () {
-    let shownAnswer = document.querySelector(".show-anser");
-    let selectedArrow = document.querySelector(".rotate-arrow");
-    if (shownAnswer) {
-      shownAnswer.classList.remove("show-anser");
-      selectedArrow.classList.remove("rotate-arrow");
-    }
-    let answer = questionBtns[i].nextElementSibling;
-    if (answer != shownAnswer) {
-      arrows[i].classList.add("rotate-arrow");
-      answer.classList.add("show-anser");
-    }
+  btn.addEventListener("click", () => {
+    showAnswer(i);
   });
 });
+
+function reset() {
+  const shownAnswer = document.querySelector(".show-anser");
+  const selectedArrow = document.querySelector(".rotate-arrow");
+  const selectedBtn = document.querySelector(".question--active");
+  if (shownAnswer) {
+    selectedBtn.classList.remove("question--active");
+    shownAnswer.classList.remove("show-anser");
+    selectedArrow.classList.remove("rotate-arrow");
+    return shownAnswer;
+  }
+}
+
+function showAnswer(i) {
+  const shownAnswer = reset();
+  const answer = asnwers[i];
+  if (answer != shownAnswer) {
+    questionBtns[i].classList.add("question--active");
+    arrows[i].classList.add("rotate-arrow");
+    answer.classList.add("show-anser");
+  }
+}
